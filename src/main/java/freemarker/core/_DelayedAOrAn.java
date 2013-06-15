c908152d0180c9dd0b480b743b59bc4dc6bd7822
ddekany <ddekany@freemail.hu>
2013-06-16 01:18:32 +0200

    (Javadoc typos...)

--- src/main/java/freemarker/core/_DelayedAOrAn.java
+++ src/main/java/freemarker/core/_DelayedAOrAn.java
@@ -1,5 +1,6 @@
 package freemarker.core;
 
+/** Don't use this; used internally by FreeMarker, might changes without notice. */
 public class _DelayedAOrAn extends _DelayedConversionToString {
 
     public _DelayedAOrAn(Object object) {
--- src/main/java/freemarker/core/_DelayedJQuote.java
+++ src/main/java/freemarker/core/_DelayedJQuote.java
@@ -2,7 +2,7 @@ package freemarker.core;
 
 import freemarker.template.utility.StringUtil;
 
-/** Don't use; used internally by FreeMarker, might changes without notice. */
+/** Don't use this; used internally by FreeMarker, might changes without notice. */
 public class _DelayedJQuote extends _DelayedConversionToString {
 
     public _DelayedJQuote(Object object) {
