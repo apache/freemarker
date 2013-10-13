4598a30417270748bf98a21027b3ce0d2d7341c8
ddekany <ddekany@freemail.hu>
2013-10-13 19:05:00 +0200

    Add minimal test for useCache BeansWrapper property.

---
+++ src/test/java/freemarker/ext/beans/ModelCacheTest.java
@@ -0,0 +1,47 @@
+package freemarker.ext.beans;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import freemarker.template.TemplateModel;
+import freemarker.template.Version;
+
+@RunWith(JUnit4.class)
+public class ModelCacheTest {
+    
+    @Test
+    public void moldeCacheOff() throws Exception {
+        BeansWrapper bw = BeansWrapper.getInstance(new Version(2, 3, 21));
+        assertFalse(bw.getUseCache());  // default is off
+        
+        String s = "foo";
+        assertNotSame(bw.wrap(s), bw.wrap(s));
+        
+        C c = new C();
+        assertNotSame(bw.wrap(c), bw.wrap(c));
+    }
+    
+    @Test
+    public void moldeCacheOn() throws Exception {
+        BeansWrapper bw = new BeansWrapper(new Version(2, 3, 21));
+        bw.setUseCache(true);
+        assertTrue(bw.getUseCache());
+        
+        String s = "foo";
+        assertSame(bw.wrap(s), bw.wrap(s));
+        
+        C c = new C();
+        TemplateModel wrappedC = bw.wrap(c);
+        assertSame(wrappedC, bw.wrap(c));
+        
+        bw.clearClassIntrospecitonCache();
+        assertNotSame(wrappedC, bw.wrap(c));
+        assertSame(bw.wrap(c), bw.wrap(c));
+    }
+
+    static public class C { }
+    
+}
