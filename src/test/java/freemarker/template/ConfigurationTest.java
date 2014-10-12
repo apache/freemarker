ec450a2717c974c9b083e837a8e688399248fe86
ddekany <ddekany@freemail.hu>
2014-10-12 11:56:33 +0200

    (Test fix: extraInfo can be null)

--- src/test/java/freemarker/template/ConfigurationTest.java
+++ src/test/java/freemarker/template/ConfigurationTest.java
@@ -146,7 +146,6 @@ public class ConfigurationTest extends TestCase{
     public void testVersion() {
         Version v = Configuration.getVersion();
         assertTrue(v.intValue() > _TemplateAPI.VERSION_INT_2_3_20);
-        assertNotNull(v.getExtraInfo());
         assertSame(v.toString(), Configuration.getVersionNumber());
         
         try {
