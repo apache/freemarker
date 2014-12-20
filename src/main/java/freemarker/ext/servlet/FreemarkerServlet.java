5a446570a98343a69e66ad6d50ddc85f695cffb2
ddekany <ddekany@freemail.hu>
2014-12-20 18:44:42 +0100

    (Little cleanup...)

--- src/main/java/freemarker/ext/servlet/FreemarkerServlet.java
+++ src/main/java/freemarker/ext/servlet/FreemarkerServlet.java
@@ -594,22 +594,7 @@ public class FreemarkerServlet extends HttpServlet
 
         ServletContext servletContext = getServletContext();
         try {
-            if (wrapper != config.getObjectWrapper() && !objectWrapperMismatchWarnLogged && LOG.isWarnEnabled()) {
-                final boolean logWarn;
-                // Deliberate double check locking.
-                synchronized (this) {
-                    logWarn = !objectWrapperMismatchWarnLogged;
-                    if (logWarn) {
-                        objectWrapperMismatchWarnLogged = true;
-                    }
-                }
-                if (logWarn) {
-                    LOG.warn(
-                            this.getClass().getName()
-                            + ".wrapper != config.getObjectWrapper(); possibly the result of incorrect extension of "
-                            + FreemarkerServlet.class.getName() + ".");
-                }
-            }
+            logWarnOnObjectWrapperMismatch();
             
             TemplateModel model = createModel(wrapper, servletContext, request, response);
 
@@ -638,6 +623,25 @@ public class FreemarkerServlet extends HttpServlet
                     // Ignored; see above
                 }
                 throw e;
+            }
+        }
+    }
+
+    private void logWarnOnObjectWrapperMismatch() {
+        // Deliberately uses double check locking.
+        if (wrapper != config.getObjectWrapper() && !objectWrapperMismatchWarnLogged && LOG.isWarnEnabled()) {
+            final boolean logWarn;
+            synchronized (this) {
+                logWarn = !objectWrapperMismatchWarnLogged;
+                if (logWarn) {
+                    objectWrapperMismatchWarnLogged = true;
+                }
+            }
+            if (logWarn) {
+                LOG.warn(
+                        this.getClass().getName()
+                        + ".wrapper != config.getObjectWrapper(); possibly the result of incorrect extension of "
+                        + FreemarkerServlet.class.getName() + ".");
             }
         }
     }
