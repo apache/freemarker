122f426d607f256a4cb2c7689a726abc1810f434
ddekany <ddekany@freemail.hu>
2015-04-04 22:47:38 +0200

    (Continued: Test for keeping Template.getEncoding() behavior BC.)

--- src/test/java/freemarker/core/TemplatGetEncodingTest.java
+++ src/test/java/freemarker/core/TemplatGetEncodingTest.java
@@ -17,11 +17,12 @@ public class TemplatGetEncodingTest {
 
     @Test
     public void test() throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, IOException {
-        Configuration cfg = new Configuration(Configuration.VERSION_2_3_0);
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_22);
         {
             cfg.setDefaultEncoding("ISO-8859-2");
             StringTemplateLoader tl = new StringTemplateLoader();
             tl.putTemplate("t", "test");
+            tl.putTemplate("tnp", "<#test>");
             cfg.setTemplateLoader(tl);
             cfg.setCacheStorage(new StrongCacheStorage());
         }
@@ -31,12 +32,27 @@ public class TemplatGetEncodingTest {
             assertEquals("ISO-8859-2", tDefEnc.getEncoding());
             assertSame(tDefEnc, cfg.getTemplate("t"));
 
+            Template tDefEnc2 = cfg.getTemplate("t", (String) null);
+            assertEquals("ISO-8859-2", tDefEnc2.getEncoding());
+            assertSame(tDefEnc, tDefEnc2);
+            
             Template tUTF8 = cfg.getTemplate("t", "UTF-8");
             assertEquals("UTF-8", tUTF8.getEncoding());
             assertSame(tUTF8, cfg.getTemplate("t", "UTF-8"));
             assertNotSame(tDefEnc, tUTF8);
         }
 
+        {
+            Template tDefEnc = cfg.getTemplate("tnp", null, null, false);
+            assertEquals("ISO-8859-2", tDefEnc.getEncoding());
+            assertSame(tDefEnc, cfg.getTemplate("tnp", null, null, false));
+
+            Template tUTF8 = cfg.getTemplate("tnp", null, "UTF-8", false);
+            assertEquals("UTF-8", tUTF8.getEncoding());
+            assertSame(tUTF8, cfg.getTemplate("tnp", null, "UTF-8", false));
+            assertNotSame(tDefEnc, tUTF8);
+        }
+        
         {
             Template nonStoredT = new Template(null, "test", cfg);
             assertNull(nonStoredT.getEncoding());
