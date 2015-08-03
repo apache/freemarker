fb7edd145c747fe2b773ab219e1c9b31f1f71d97
ddekany <ddekany@freemail.hu>
2015-08-03 15:12:07 +0200

    (More FMParser API backward compatibility test)

--- src/test/java/freemarker/core/LegacyFMParserConstructorsTest.java
+++ src/test/java/freemarker/core/LegacyFMParserConstructorsTest.java
@@ -1,6 +1,11 @@
 package freemarker.core;
 
+import java.io.IOException;
+
 import org.junit.Test;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 
 public class LegacyFMParserConstructorsTest {
 
@@ -8,6 +13,19 @@ public class LegacyFMParserConstructorsTest {
     public void test1() throws ParseException {
         FMParser parser = new FMParser("x");
         parser.Root();
+    }
+    
+    @Test
+    public void testCreateExpressionParser() throws ParseException {
+         FMParser parser = FMParser.createExpressionParser("x + y");
+         parser.Expression();
+    }
+
+    @Test
+    public void testCreateExpressionParser2() throws IOException {
+         FMParser parser = FMParser.createExpressionParser("x + 1");
+         parser.setTemplate(new Template(null, "", new Configuration()));
+         parser.Expression();
     }
     
 }
