fca87ab1b4db66c63637c195811d8a6e999ac244
ddekany <ddekany@freemail.hu>
2015-08-24 16:47:59 +0200

    Manual: Continued documenting the major 2.3.24 features (incomplete).

--- src/test/java/freemarker/manual/AutoEscapingExample.java
+++ src/test/java/freemarker/manual/AutoEscapingExample.java
@@ -18,5 +18,30 @@ public class AutoEscapingExample extends ExamplesTest {
     public void testMarkup() throws Exception {
         assertOutputForNamed("AutoEscapingExample-markup.ftlh");
     }
+
+    @Test
+    public void testConvert() throws Exception {
+        assertOutputForNamed("AutoEscapingExample-convert.ftlh");
+    }
+
+    @Test
+    public void testConvert2() throws Exception {
+        assertOutputForNamed("AutoEscapingExample-convert2.ftl");
+    }
+
+    @Test
+    public void testStringLiteral() throws Exception {
+        assertOutputForNamed("AutoEscapingExample-stringLiteral.ftlh");
+    }
+
+    @Test
+    public void testStringLiteral2() throws Exception {
+        assertOutputForNamed("AutoEscapingExample-stringLiteral2.ftlh");
+    }
+
+    @Test
+    public void testStringConcat() throws Exception {
+        assertOutputForNamed("AutoEscapingExample-stringConcat.ftlh");
+    }
     
 }
