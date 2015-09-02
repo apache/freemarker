c0a231d3f1d481f849bd69ff97853f4fec250f6f
ddekany <ddekany@apache.org>
2015-09-02 22:29:26 +0200

    Changed owner in copyright headers to the Apache Software Foundation. Modified/created NOTICE, LICENSE and DISCLAIMER files, so that the owner there is the Apache Software Foundation too, and so that their file name and content complies with the guidelines provided by the Apache Software Foundation.

--- src/main/java/freemarker/core/CommandLine.java
+++ src/main/java/freemarker/core/CommandLine.java
@@ -1,17 +1,20 @@
 /*
- * Copyright 2014 Attila Szegedi, Daniel Dekany, Jonathan Revusky
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package freemarker.core;
@@ -55,7 +58,7 @@ public class CommandLine {
         }
         
         System.out.println();
-        System.out.println("Copyright (c) 2014 Attila Szegedi, Daniel Dekany, Jonathan Revusky.");
+        System.out.println("Copyright 2015 The Apache Software Foundation.");
         System.out.println("Licensed under the Apache License, Version 2.0");
         System.out.println();
         System.out.println("For more information and for updates visit our Web site:");
