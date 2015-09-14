2daf5f3d78d15cfed55b93459ef31af9a1889db9
ddekany <ddekany@apache.org>
2015-09-14 02:23:29 +0200

    Added support for calling some Java 6 functions (despite that we only require Java 5)

---
+++ src/main/java/freemarker/core/_Java6.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package freemarker.core;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+/**
+ * Used internally only, might changes without notice!
+ * Used for accessing functionality that's only present in Java 6 or later.
+ */
+public interface _Java6 {
+
+    void setRoundingMode(DecimalFormat df, RoundingMode roundingMode);
+
+    void setExponentSeparator(DecimalFormatSymbols dfs, String exponentSeparator);
+    
+}
---
+++ src/main/java/freemarker/core/_Java6Impl.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package freemarker.core;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+/**
+ * Used internally only, might changes without notice!
+ * Used for accessing functionality that's only present in Java 6 or later.
+ */
+public final class _Java6Impl implements _Java6 {
+    
+    public static final _Java6 INSTANCE = new _Java6Impl();
+
+    private _Java6Impl() {
+        // Not meant to be instantiated
+    }
+
+    public void setRoundingMode(DecimalFormat df, RoundingMode roundingMode) {
+        df.setRoundingMode(roundingMode);
+    }
+
+    public void setExponentSeparator(DecimalFormatSymbols dfs, String exponentSeparator) {
+        dfs.setExponentSeparator(exponentSeparator);
+    }
+    
+}
