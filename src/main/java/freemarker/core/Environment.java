e3d1bb837df300d4b9230716cc391199a599e109
ddekany <ddekany@apache.org>
2017-08-05 10:03:39 +0200

    (JavaDoc adjustment)

--- src/main/java/freemarker/core/Environment.java
+++ src/main/java/freemarker/core/Environment.java
@@ -242,8 +242,7 @@ public final class Environment extends Configurable {
     /**
      * Returns the {@link Template} that we are "lexically" inside at the moment. This template will change when
      * entering an {@code #include} or calling a macro or function in another template, or returning to yet another
-     * template with {@code #nested}. As such, it's useful in {@link TemplateDirectiveModel} to find out if from where
-     * the directive was called from.
+     * template with {@code #nested}.
      * 
      * @see #getMainTemplate()
      * @see #getCurrentNamespace()
