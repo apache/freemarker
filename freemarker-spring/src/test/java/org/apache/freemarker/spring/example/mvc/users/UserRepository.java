96c038d9ccc4a5d38cc8896370870d27af46af1f
Woonsan Ko <woonsan@apache.org>
2017-09-11 23:21:50 -0400

    FREEMARKER-55: unit test template code

---
+++ freemarker-spring/src/test/java/org/apache/freemarker/spring/example/mvc/users/UserRepository.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.freemarker.spring.example.mvc.users;
+
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+    private Map<String, User> usersMap = new ConcurrentHashMap<>();
+    {
+        String id = "13c2ec8c-102c-4883-a282-3fe983e61515";
+        User user = new User(id);
+        user.setEmail("john@example.com");
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        Calendar birthDate = Calendar.getInstance();
+        birthDate.set(Calendar.YEAR, 1971);
+        birthDate.set(Calendar.MONTH, Calendar.JANUARY);
+        birthDate.set(Calendar.DATE, 5);
+        user.setBirthDate(birthDate.getTime());
+        usersMap.put(id, user);
+
+        id = "04d6080b-2098-4eaf-90ee-7331caab5e91";
+        user = new User(id);
+        user.setEmail("jane@example.com");
+        user.setFirstName("Jane");
+        user.setLastName("Doe");
+        birthDate = Calendar.getInstance();
+        birthDate.set(Calendar.YEAR, 1970);
+        birthDate.set(Calendar.MONTH, Calendar.FEBRUARY);
+        birthDate.set(Calendar.DATE, 7);
+        user.setBirthDate(birthDate.getTime());
+        usersMap.put(id, user);
+    }
+
+    public synchronized Set<String> getUserIds() {
+        return new TreeSet<>(usersMap.keySet());
+    }
+
+    public synchronized User getUser(final String id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID must be non-null.");
+        }
+
+        User user = usersMap.get(id);
+
+        if (user != null) {
+            return cloneUser(user, user.getId());
+        }
+
+        return null;
+    }
+
+    public synchronized User addOrUpdateUser(final User user) {
+        final String id = user.getId();
+        User newUser = cloneUser(user, id);
+        usersMap.put(id, newUser);
+        return cloneUser(newUser, id);
+    }
+
+    public synchronized boolean deleteUser(final String username) {
+        if (username == null) {
+            throw new IllegalArgumentException("Username must be non-null.");
+        }
+
+        final User user = usersMap.remove(username);
+        return user != null;
+    }
+
+    private User cloneUser(final User source, final String id) {
+        User clone = new User(id);
+        clone.setPassword(source.getPassword());
+        clone.setEmail(source.getEmail());
+        clone.setFirstName(source.getFirstName());
+        clone.setLastName(source.getLastName());
+        clone.setBirthDate(source.getBirthDate());
+        return clone;
+    }
+}
---
+++ freemarker-spring/src/test/java/org/apache/freemarker/spring/model/MessageFunctionTest.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.freemarker.spring.model;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration("classpath:META-INF/web-resources")
+@ContextConfiguration("MessageFunctionTest-context.xml")
+public class MessageFunctionTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void getUsers() throws Exception {
+        mockMvc.perform(get("/users").accept(MediaType.parseMediaType("text/html")))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith("text/html"))
+            .andExpect(xpath("/html/head/title").string("Spring MVC Form Example - Users"));
+    }
+}
