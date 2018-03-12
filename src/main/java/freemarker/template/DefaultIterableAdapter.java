15e8ac35bbcb652c49c55b88d7c7496834f03f71
ddekany <ddekany@apache.org>
2018-03-12 06:38:29 +0100

    (JavaDoc typo)

--- src/main/java/freemarker/template/DefaultIterableAdapter.java
+++ src/main/java/freemarker/template/DefaultIterableAdapter.java
@@ -33,7 +33,7 @@ import freemarker.template.utility.ObjectWrapperWithAPISupport;
  * 
  * <p>
  * Thread safety: A {@link DefaultIterableAdapter} is as thread-safe as the {@link Iterable} that it wraps is. Normally
- * you only have to consider read-only access, as the FreeMarker template language doesn't provide mean to call
+ * you only have to consider read-only access, as the FreeMarker template language doesn't provide means to call
  * {@link Iterator} modifier methods (though of course, Java methods called from the template can violate this rule).
  * 
  * <p>
