088cef6369a55d08220859b1abc18f80f5be2508
ddekany <ddekany@apache.org>
2018-03-24 09:51:10 +0100

    Forward ported from 2.3-gae: Parser error message test changes

--- freemarker-core-test/src/test/java/org/apache/freemarker/core/ParsingErrorMessagesTest.java
+++ freemarker-core-test/src/test/java/org/apache/freemarker/core/ParsingErrorMessagesTest.java
@@ -36,159 +36,163 @@ public class ParsingErrorMessagesTest extends TemplateTest {
 
     @Test
     public void testNeedlessInterpolation() {
-        assertErrorContains("<#if ${x} == 3></#if>", "instead of ${");
-        assertErrorContains("<#if ${x == 3}></#if>", "instead of ${");
-        assertErrorContains("<@foo ${x == 3} />", "instead of ${");
+        assertErrorContainsAS("<#if ${x} == 3></#if>", "instead of ${");
+        assertErrorContainsAS("<#if ${x == 3}></#if>", "instead of ${");
+        assertErrorContainsAS("<@foo ${x == 3} />", "instead of ${");
+        
+        setConfiguration(new TestConfigurationBuilder()
+                .interpolationSyntax(InterpolationSyntax.SQUARE_BRACKET)
+                .build());
+        assertErrorContains("<@foo [= x == 3] />", "instead of [=");
     }
 
     @Test
     public void testWrongDirectiveNames() {
-        assertErrorContains("<#foo />", "nknown directive", "#foo");
-        assertErrorContains("<#set x = 1 />", "nknown directive", "#set", "#assign");
-        assertErrorContains("<#iterator></#iterator>", "nknown directive", "#iterator", "#list");
-        assertErrorContains("<#if x><#elseif y></#if>", "nknown directive", "#elseIf");
-        assertErrorContains("<#outputformat 'HTML'></#outputformat>", "nknown directive", "#outputFormat");
-        assertErrorContains("<#autoesc></#autoesc>", "nknown directive", "#autoEsc");
-        assertErrorContains("<#noautoesc></#noautoesc>", "nknown directive", "#noAutoEsc");
+        assertErrorContainsAS("<#foo />", "nknown directive", "#foo");
+        assertErrorContainsAS("<#set x = 1 />", "nknown directive", "#set", "#assign");
+        assertErrorContainsAS("<#iterator></#iterator>", "nknown directive", "#iterator", "#list");
+        assertErrorContainsAS("<#if x><#elseif y></#if>", "nknown directive", "#elseIf");
+        assertErrorContainsAS("<#outputformat 'HTML'></#outputformat>", "nknown directive", "#outputFormat");
+        assertErrorContainsAS("<#autoesc></#autoesc>", "nknown directive", "#autoEsc");
+        assertErrorContainsAS("<#noautoesc></#noautoesc>", "nknown directive", "#noAutoEsc");
     }
 
     @Test
     public void testBug402() {
-        assertErrorContains("<#list 1..i as k>${k}<#list>", "existing directive", "malformed", "#list");
-        assertErrorContains("<#assign>", "existing directive", "malformed", "#assign");
-        assertErrorContains("</#if x>", "existing directive", "malformed", "#if");
-        assertErrorContains("<#compress x>", "existing directive", "malformed", "#compress");
+        assertErrorContainsAS("<#list 1..i as k>${k}<#list>", "existing directive", "malformed", "#list");
+        assertErrorContainsAS("<#assign>", "existing directive", "malformed", "#assign");
+        assertErrorContainsAS("</#if x>", "existing directive", "malformed", "#if");
+        assertErrorContainsAS("<#compress x>", "existing directive", "malformed", "#compress");
     }
 
     @Test
     public void testUnclosedDirectives() {
-        assertErrorContains("<#macro x>", "#macro", "unclosed");
-        assertErrorContains("<#macro x></#function>", "macro end tag");
-        assertErrorContains("<#function x()>", "#macro", "unclosed");
-        assertErrorContains("<#function x()></#macro>", "function end tag");
-        assertErrorContains("<#assign x>", "#assign", "unclosed");
-        assertErrorContains("<#macro m><#local x>", "#local", "unclosed");
-        assertErrorContains("<#global x>", "#global", "unclosed");
-        assertErrorContains("<@foo>", "@...", "unclosed");
-        assertErrorContains("<#list xs as x>", "#list", "unclosed");
-        assertErrorContains("<#list xs as x><#if x>", "#if", "unclosed");
-        assertErrorContains("<#list xs as x><#if x><#if q><#else>", "#if", "unclosed");
-        assertErrorContains("<#list xs as x><#if x><#if q><#else><#macro x>qwe", "#macro", "unclosed");
-        assertErrorContains("${(blah", "\"(\"", "unclosed");
-        assertErrorContains("${blah", "\"{\"", "unclosed");
+        assertErrorContainsAS("<#macro x>", "#macro", "unclosed");
+        assertErrorContainsAS("<#macro x></#function>", "macro end tag");
+        assertErrorContainsAS("<#function x()>", "#macro", "unclosed");
+        assertErrorContainsAS("<#function x()></#macro>", "function end tag");
+        assertErrorContainsAS("<#assign x>", "#assign", "unclosed");
+        assertErrorContainsAS("<#macro m><#local x>", "#local", "unclosed");
+        assertErrorContainsAS("<#global x>", "#global", "unclosed");
+        assertErrorContainsAS("<@foo>", "@...", "unclosed");
+        assertErrorContainsAS("<#list xs as x>", "#list", "unclosed");
+        assertErrorContainsAS("<#list xs as x><#if x>", "#if", "unclosed");
+        assertErrorContainsAS("<#list xs as x><#if x><#if q><#else>", "#if", "unclosed");
+        assertErrorContainsAS("<#list xs as x><#if x><#if q><#else><#macro x>qwe", "#macro", "unclosed");
+        assertErrorContainsAS("${(blah", "\"(\"", "unclosed");
+        assertErrorContainsAS("${blah", "\"{\"", "unclosed");
     }
 
     @Test
     public void testBuiltInWrongNames() {
-        assertErrorContains("${x?lower_case}", "camel case", "The correct name is: lowerCase");
-        assertErrorContains("${x?iso_utc_nz}", "camel case", "The correct name is: isoUtcNZ");
-        assertErrorContains("${x?no_such_name}", "camel case", "\\!The correct name is:", "alphabetical list");
-        assertErrorContains("${x?nosuchname}", "\\!camel case", "\\!The correct name is:", "alphabetical list");
-        assertErrorContains("${x?datetime}", "The correct name is: dateTime");
-        assertErrorContains("${x?datetimeIfUnknown}", "The correct name is: dateTimeIfUnknown");
-        assertErrorContains("${x?datetime_if_unknown}", "The correct name is: dateTimeIfUnknown");
-        assertErrorContains("${x?exists}", "someExpression??");
-        assertErrorContains("${x?if_exists}", "someExpression!");
-        assertErrorContains("${x?default(1)}", "someExpression!defaultExpression");
+        assertErrorContainsAS("${x?lower_case}", "camel case", "The correct name is: lowerCase");
+        assertErrorContainsAS("${x?iso_utc_nz}", "camel case", "The correct name is: isoUtcNZ");
+        assertErrorContainsAS("${x?no_such_name}", "camel case", "\\!The correct name is:", "alphabetical list");
+        assertErrorContainsAS("${x?nosuchname}", "\\!camel case", "\\!The correct name is:", "alphabetical list");
+        assertErrorContainsAS("${x?datetime}", "The correct name is: dateTime");
+        assertErrorContainsAS("${x?datetimeIfUnknown}", "The correct name is: dateTimeIfUnknown");
+        assertErrorContainsAS("${x?datetime_if_unknown}", "The correct name is: dateTimeIfUnknown");
+        assertErrorContainsAS("${x?exists}", "someExpression??");
+        assertErrorContainsAS("${x?if_exists}", "someExpression!");
+        assertErrorContainsAS("${x?default(1)}", "someExpression!defaultExpression");
     }
 
     @Test
     public void testSettingWrongNames() {
-        assertErrorContains("<#setting time_format='HHmm'>", "camel case", "The correct name is: timeFormat",
+        assertErrorContainsAS("<#setting time_format='HHmm'>", "camel case", "The correct name is: timeFormat",
                 "\\!setting names are:");
-        assertErrorContains("<#setting no_such_name=1>", "camel case", "\\!The correct name is:",
+        assertErrorContainsAS("<#setting no_such_name=1>", "camel case", "\\!The correct name is:",
                 "setting names are:");
-        assertErrorContains("<#setting nosuchname=1>", "\\!The correct name is:", "\\!camel case",
+        assertErrorContainsAS("<#setting nosuchname=1>", "\\!The correct name is:", "\\!camel case",
                 "setting names are:");
 
-        assertErrorContains("<#setting datetime_format='HHmm'>", "The correct name is: dateTimeFormat");
-        assertErrorContains("<#setting datetimeFormat='HHmm'>", "The correct name is: dateTimeFormat");
+        assertErrorContainsAS("<#setting datetime_format='HHmm'>", "The correct name is: dateTimeFormat");
+        assertErrorContainsAS("<#setting datetimeFormat='HHmm'>", "The correct name is: dateTimeFormat");
     }
 
     @Test
     public void testSpecialVariableWrongNames() {
-        assertErrorContains("${.data_model}", "camel case", "The correct name is: dataModel",
+        assertErrorContainsAS("${.data_model}", "camel case", "The correct name is: dataModel",
                 "\\!variable names are:");
-        assertErrorContains("${.no_such_name}", "camel case", "\\!The correct name is:",
+        assertErrorContainsAS("${.no_such_name}", "camel case", "\\!The correct name is:",
                 "variable names are:");
-        assertErrorContains("${.nosuchname}", "\\!camel case", "\\!The correct name is:",
+        assertErrorContainsAS("${.nosuchname}", "\\!camel case", "\\!The correct name is:",
                 "variable names are:");
     }
 
     @Test
     public void testFtlParameterWrongNames() {
-        assertErrorContains("<#ftl strip_whitespace=false>", "camel case", "The correct name is: stripWhitespace");
-        assertErrorContains("<#ftl no_such_name=1>", "camel case", "\\!The correct name is:");
-        assertErrorContains("<#ftl nosuchname=1>", "\\!camel case", "\\!The correct name is:");
+        assertErrorContainsAS("<#ftl strip_whitespace=false>", "camel case", "The correct name is: stripWhitespace");
+        assertErrorContainsAS("<#ftl no_such_name=1>", "camel case", "\\!The correct name is:");
+        assertErrorContainsAS("<#ftl nosuchname=1>", "\\!camel case", "\\!The correct name is:");
     }
 
     @Test
     public void testInterpolatingClosingsErrors() throws Exception {
-        assertErrorContains("<#ftl>${x", "unclosed");        
-        assertErrorContains("<#assign x = x}>", "\"}\"", "open");
-        assertErrorContains("<#ftl>${'x']", "\"]\"", "open");
-        super.assertErrorContains("<#ftl>${'x'>", "end of file");
-        super.assertErrorContains("[#ftl]${'x'>", "end of file");
+        assertErrorContainsAS("<#ftl>${x", "unclosed");        
+        assertErrorContainsAS("<#assign x = x}>", "\"}\"", "open");
+        assertErrorContainsAS("<#ftl>${'x']", "\"]\"", "open");
+        assertErrorContains("<#ftl>${'x'>", "end of file");
+        assertErrorContains("[#ftl]${'x'>", "end of file");
         
         assertOutput("<#assign x = '${x'>", ""); // TODO [FM3] Legacy glitch... should fail in theory.
     }
 
     @Test
     public void testUnknownHeaderParameter() {
-        assertErrorContains("<#ftl foo=1>", "Unknown", "foo");
-        assertErrorContains("<#ftl attributes={}>", "Unknown", "attributes", "customSettings");
+        assertErrorContainsAS("<#ftl foo=1>", "Unknown", "foo");
+        assertErrorContainsAS("<#ftl attributes={}>", "Unknown", "attributes", "customSettings");
     }
 
     @Test
     public void testDynamicTopCalls() throws IOException, TemplateException {
-        assertErrorContains("<@a, n1=1 />", "Remove comma", "between", "by position");
-        assertErrorContains("<@a n1=1, n2=1 />", "Remove comma", "between", "by position");
-        assertErrorContains("<@a n1=1, 2 />", "Remove comma", "between", "by position");
-        assertErrorContains("<@a, 1 />", "Remove comma", "between", "by position");
-        assertErrorContains("<@a 1, , 2 />", "Two commas");
-        assertErrorContains("<@a 1 2 />", "Missing comma");
-        assertErrorContains("<@a n1=1 2 />", "must be earlier than arguments passed by name");
+        assertErrorContainsAS("<@a, n1=1 />", "Remove comma", "between", "by position");
+        assertErrorContainsAS("<@a n1=1, n2=1 />", "Remove comma", "between", "by position");
+        assertErrorContainsAS("<@a n1=1, 2 />", "Remove comma", "between", "by position");
+        assertErrorContainsAS("<@a, 1 />", "Remove comma", "between", "by position");
+        assertErrorContainsAS("<@a 1, , 2 />", "Two commas");
+        assertErrorContainsAS("<@a 1 2 />", "Missing comma");
+        assertErrorContainsAS("<@a n1=1 2 />", "must be earlier than arguments passed by name");
     }
 
     @Test
     public void testMacroAndFunctionDefinitions() {
-        assertErrorContains("<#macro m><#macro n></#macro></#macro>", "nested into each other");
-        assertErrorContains("<#macro m(a)></#macro>", "can't use \"(\"");
-        assertErrorContains("<#function f a></#function>", "must use \"(\"");
-        assertErrorContains("<#macro m a{badOption})></#macro>", "\"badOption\"",
+        assertErrorContainsAS("<#macro m><#macro n></#macro></#macro>", "nested into each other");
+        assertErrorContainsAS("<#macro m(a)></#macro>", "can't use \"(\"");
+        assertErrorContainsAS("<#function f a></#function>", "must use \"(\"");
+        assertErrorContainsAS("<#macro m a{badOption})></#macro>", "\"badOption\"",
                 "\"" + ASTDirMacroOrFunction.POSITIONAL_PARAMETER_OPTION_NAME + "\"",
                 "\"" + ASTDirMacroOrFunction.NAMED_PARAMETER_OPTION_NAME + "\"");
-        assertErrorContains("<#function f(a{named}, b)></#function>", "Positional", "must precede named");
-        assertErrorContains("<#function f(a..., b)></#function>", "another", "after", "positional varargs");
-        assertErrorContains("<#function f(a..., b...)></#function>", "another", "after", "positional varargs");
-        assertErrorContains("<#macro m a... b></#macro>", "another", "after", "named varargs");
-        assertErrorContains("<#macro m a... b...></#macro>", "another", "after", "named varargs");
-        assertErrorContains("<#function f(a b)></#function>", "Function", "must have comma");
-        assertErrorContains("<#function f(a{named} b{named})></#function>", "Function", "must have comma");
-        assertErrorContains("<#macro m a, b></#macro>", "Named param", "macro", "need no comma");
-        assertErrorContains("<#macro m a{positional} b{positional}></#macro>",
+        assertErrorContainsAS("<#function f(a{named}, b)></#function>", "Positional", "must precede named");
+        assertErrorContainsAS("<#function f(a..., b)></#function>", "another", "after", "positional varargs");
+        assertErrorContainsAS("<#function f(a..., b...)></#function>", "another", "after", "positional varargs");
+        assertErrorContainsAS("<#macro m a... b></#macro>", "another", "after", "named varargs");
+        assertErrorContainsAS("<#macro m a... b...></#macro>", "another", "after", "named varargs");
+        assertErrorContainsAS("<#function f(a b)></#function>", "Function", "must have comma");
+        assertErrorContainsAS("<#function f(a{named} b{named})></#function>", "Function", "must have comma");
+        assertErrorContainsAS("<#macro m a, b></#macro>", "Named param", "macro", "need no comma");
+        assertErrorContainsAS("<#macro m a{positional} b{positional}></#macro>",
                 "Positional param", "must have comma");
-        assertErrorContains("<#macro m a...=[]></#macro>", "Varargs", "default");
-        assertErrorContains("<#function f(a=0, b)></#function>", "with default", "without a default");
-        assertErrorContains("<#function f(a,)></#function>", "Comma without");
-        assertErrorContains("<#macro m a{positional}, b{positional},></#macro>", "Comma without");
-        super.assertErrorContains("<#function f(a, b></#function>");
-        super.assertErrorContains("<#function f(></#function>");
-        super.assertErrorContains("[#ftl][#function f(a, b][/#function]", "Missing closing \")\"");
-        super.assertErrorContains("[#ftl][#function f(][/#function]", "Missing closing \")\"");
-        assertErrorContains("<#macro m a b)></#macro>", "\")\" without", "opening");
-        assertErrorContains("<#macro m a b a></#macro>", "\"a\"", "multiple");
+        assertErrorContainsAS("<#macro m a...=[]></#macro>", "Varargs", "default");
+        assertErrorContainsAS("<#function f(a=0, b)></#function>", "with default", "without a default");
+        assertErrorContainsAS("<#function f(a,)></#function>", "Comma without");
+        assertErrorContainsAS("<#macro m a{positional}, b{positional},></#macro>", "Comma without");
+        assertErrorContains("<#function f(a, b></#function>");
+        assertErrorContainsAS("<#function f(></#function>");
+        assertErrorContainsAS("[#ftl][#function f(a, b][/#function]", "Missing closing \")\"");
+        assertErrorContainsAS("[#ftl][#function f(][/#function]", "Missing closing \")\"");
+        assertErrorContainsAS("<#macro m a b)></#macro>", "\")\" without", "opening");
+        assertErrorContainsAS("<#macro m a b a></#macro>", "\"a\"", "multiple");
     }
 
-    @Override
-    protected Throwable assertErrorContains(String ftl, String... expectedSubstrings) {
-        super.assertErrorContains(ftl, expectedSubstrings);
-        ftl = ftl.replace('<', '[').replace('>', ']');
-        return super.assertErrorContains(ftl, expectedSubstrings);
-    }
-
-    private void showError(Throwable e) {
-        //System.out.println(e);
+    /**
+     * "assertErrorContains" with both angle bracket and square bracket tag syntax, by converting the input tag syntax.
+     * Beware, it uses primitive search-and-replace.
+     */
+    protected Throwable assertErrorContainsAS(String angleBracketsFtl, String... expectedSubstrings) {
+        assertErrorContains(angleBracketsFtl, expectedSubstrings);
+        angleBracketsFtl = angleBracketsFtl.replace('<', '[').replace('>', ']');
+        return assertErrorContains(angleBracketsFtl, expectedSubstrings);
     }
 
 }
