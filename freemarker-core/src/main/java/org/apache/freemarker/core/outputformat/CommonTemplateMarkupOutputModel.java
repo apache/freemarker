0f3701a2f8f5c6ab77612449e1b32a0c70f35c68
ddekany <ddekany@apache.org>
2018-12-19 11:17:05 +0100

    Forward ported from 2.3-gae: Better toString() for the commonly used TemplateMarkupOutputModel-s.

--- freemarker-core/src/main/java/org/apache/freemarker/core/outputformat/CommonTemplateMarkupOutputModel.java
+++ freemarker-core/src/main/java/org/apache/freemarker/core/outputformat/CommonTemplateMarkupOutputModel.java
@@ -64,4 +64,15 @@ public abstract class CommonTemplateMarkupOutputModel<MO extends CommonTemplateM
         this.markupContent = markupContent;
     }
 
+    /**
+     * Returns something like {@code "markup(format=HTML, markup=<p>foo</p>)"}; where the first parameter is
+     * {@link OutputFormat#getName()}, and the second is the content, that's prefixed with {@code markup=} or {@code
+     * plainText=}, depending on the way the content is internally stored.
+     */
+    @Override
+    public String toString() {
+        return "markupOutput(format=" + getOutputFormat().getName() + ", " + (plainTextContent != null ?
+                "plainText=" + plainTextContent : "markup=" + markupContent) + ")";
+    }
+
 }
