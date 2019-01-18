81a6b2d5a3c82869be665771beed27a9aa091678
ddekany <ddekany@apache.org>
2019-01-18 11:17:11 +0100

    TemplateModelWithAPISupport is not experimental anymore.

--- src/main/java/freemarker/template/TemplateModelWithAPISupport.java
+++ src/main/java/freemarker/template/TemplateModelWithAPISupport.java
@@ -22,12 +22,8 @@ package freemarker.template;
 import freemarker.template.utility.ObjectWrapperWithAPISupport;
 
 /**
- * <b>Experimental - subject to change:</b> A {@link TemplateModel} on which the {@code ?api} operation can be applied.
- * 
- * <p>
- * <b>Experimental status warning:</b> This interface is subject to change on non-backward compatible ways, hence, it
- * shouldn't be implemented outside FreeMarker yet.
- * 
+ * A {@link TemplateModel} on which the {@code ?api} operation can be applied.
+ *
  * @since 2.3.22
  */
 public interface TemplateModelWithAPISupport extends TemplateModel {
