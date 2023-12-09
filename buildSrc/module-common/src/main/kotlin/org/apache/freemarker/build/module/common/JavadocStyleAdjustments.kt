63d7c855931927418bf17a3ed701d55c6204c5d1
ddekany <ddekany@apache.org>
2023-12-09 21:59:20 +0100

    Gradle build: Do not customize javadoc output anymore - It complicates the build, has to be reviewed for new javadoc versions, and almost nobody uses the javadoc HTML-s nowadays.

--- buildSrc/module-common/src/main/kotlin/org/apache/freemarker/build/module/common/JavadocStyleAdjustments.kt
+++ buildSrc/module-common/src/main/kotlin/org/apache/freemarker/build/module/common/JavadocStyleAdjustments.kt
@@ -19,14 +19,10 @@
 
 package org.apache.freemarker.build.module.common
 
-import org.gradle.api.Action
-import org.gradle.api.Task
 import org.gradle.api.plugins.JavaBasePlugin
 import org.gradle.api.tasks.javadoc.Javadoc
 import org.gradle.external.javadoc.StandardJavadocDocletOptions
-import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets
-import java.nio.file.Files
 
 fun configureJavadocDefaults(javadoc: Javadoc) {
     javadoc.group = JavaBasePlugin.DOCUMENTATION_GROUP
@@ -47,47 +43,5 @@ fun configureJavadocDefaults(javadoc: Javadoc) {
 
         // There are too many to check
         addStringOption("Xdoclint:all,-missing", "-quiet")
-    }
-
-    javadoc.doLast(JavadocStyleAdjustments())
-}
-
-class JavadocStyleAdjustments: Action<Task> {
-    override fun execute(task: Task) {
-        val javadoc = task as Javadoc
-        val stylesheetPath = javadoc.destinationDir!!.toPath().resolve("stylesheet.css")
-        task.logger.info("Fixing JDK 8 ${stylesheetPath}")
-
-        val ddSelectorStart = "(?:\\.contentContainer\\s+\\.(?:details|description)|\\.serializedFormContainer)\\s+dl\\s+dd\\b.*?\\{[^\\}]*\\b"
-        val ddPropertyEnd = "\\b.+?;"
-
-        val fixRules = listOf(
-                Pair(Regex("/\\* (Javadoc style sheet) \\*/"), "/\\* \\1 - JDK 8 usability fix regexp substitutions applied \\*/"),
-                Pair(Regex("@import url\\('resources/fonts/dejavu.css'\\);\\s*"), ""),
-                Pair(Regex("['\"]DejaVu Sans['\"]"), "Arial"),
-                Pair(Regex("['\"]DejaVu Sans Mono['\"]"), "'Courier New'"),
-                Pair(Regex("['\"]DejaVu Serif['\"]"), "Arial"),
-                Pair(Regex("(?<=[\\s,:])serif\\b"), "sans-serif"),
-                Pair(Regex("(?<=[\\s,:])Georgia,\\s*"), ""),
-                Pair(Regex("['\"]Times New Roman['\"],\\s*"), ""),
-                Pair(Regex("(?<=[\\s,:])Times,\\s*"), ""),
-                Pair(Regex("(?<=[\\s,:])Arial\\s*,\\s*Arial\\b"), ""),
-                Pair(Regex("(${ddSelectorStart})margin${ddPropertyEnd}"), "\\1margin: 5px 0 10px 20px;"),
-                Pair(Regex("(${ddSelectorStart})font-family${ddPropertyEnd}"), "\\1")
-        )
-
-        val javadocEncoding = Charset.forName(javadoc.options.encoding)
-
-        var stylesheetContent = String(Files.readAllBytes(stylesheetPath), javadocEncoding)
-        for (rule in fixRules) {
-            val prevContent = stylesheetContent
-            stylesheetContent = stylesheetContent.replace(rule.first, rule.second)
-
-            if (prevContent == stylesheetContent) {
-                task.logger.warn("Javadoc style sheet did not contain anything matching: ${rule.first}")
-            }
-        }
-
-        Files.write(stylesheetPath, stylesheetContent.toByteArray(javadocEncoding))
     }
 }
